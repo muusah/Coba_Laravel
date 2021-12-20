9c15c83093b36fece143ff07121c0529e2641241
muusah <92143912+muusah@users.noreply.github.com>
2021-12-21 06:21:58 +0700

    commit keempat

--- coba1-app/resources/views/create.blade.php
+++ coba1-app/resources/views/create.blade.php
@@ -4,17 +4,29 @@
     <div class="row mb-4">
         <div class="col-12">
             <h5 class="mb-4">Create Todo</h5>
+            
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
-            <form>
+            <form action="{{ route('store') }}" method="POST">
+                @method('POST')
+                {{ csrf_field() }}
                 <div class="form-group">
                     <label for="example-todo-title">Tuliskan judul to-do</label>
-                    <input type="text" class="form-control" id="example-todo-title" aria-describedby="emailHelp" placeholder="Enter to-do title" required>
+                    <input value="{{ old('title') }}" name="title" type="text" class="form-control" id="example-todo-title" aria-describedby="emailHelp" placeholder="Enter to-do title" required>
                     <small id="emailHelp" class="form-text text-muted">Judul to-do minimal berisi 5 karakter.</small>
                 </div>
 
                 <div class="form-group">
                     <label for="example-todo-desc">Jelaskan deskripsi to-do</label>
-                    <textarea rows="3" class="form-control" id="example-todo-desc" aria-describedby="emailHelp" placeholder="Enter to-do description"></textarea>
+                    <textarea name="description" rows="3" class="form-control" id="example-todo-desc" aria-describedby="emailHelp" placeholder="Enter to-do description">{{ old('description') }}</textarea>
                     <small id="emailHelp" class="form-text text-muted">Kamu dapat menuliskan deskripsi to-do secara detail pada kolom ini.</small>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
