301575bf4560a03b081d855c0cfc616a0e8771c6
muusah <92143912+muusah@users.noreply.github.com>
2021-12-20 21:44:40 +0700

    commit ketiga

--- coba1-app/app/Http/Controllers/ToDoController.php
+++ coba1-app/app/Http/Controllers/ToDoController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-// use App\Models\Todo;
-// use Illuminate\Support\Facades\Date;
+use App\Models\Todo;
+use Illuminate\Support\Facades\Date;
 
 class ToDoController extends Controller
 {
